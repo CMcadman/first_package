83c104312968a6146ee5cc5602308b7fb8eabcd8
Harry <MingChenXX@163.com>
2020-12-24 21:51:46 +0800

    添加基础组件

--- lib/first_harry.dart
+++ lib/first_harry.dart
@@ -1,5 +1,8 @@
 library first_harry;
 
+import 'package:first_harry/utils/screen_adapter.dart';
+import 'package:flutter/material.dart';
+
 export 'first_test/first_test.dart';
 export 'caculator/caculator_page.dart';
 
@@ -7,4 +10,9 @@ export 'caculator/caculator_page.dart';
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+/// screen_util初始化
+class CMScreenUtils {
+  void init(BuildContext context) => ScreenAdapter.init(context);
 }
---
+++ lib/utils/as_constant.dart
@@ -0,0 +1,8 @@
+class ASConstant {
+  /// 主边距
+  static const double pEdgeInset = 15.0;
+
+  static const String adhots = 'assets/adhots/';
+
+  static const String others = 'assets/others/';
+}
